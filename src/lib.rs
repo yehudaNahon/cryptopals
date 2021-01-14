4550ce89fa9f65f2edfb900b53fd7b9092d62ff9
Yehuda Nahon <yehudanahon98@gmail.com>
2021-01-14 20:04:58 +0200

    finished set 1 challenge 5

--- src/lib.rs
+++ src/lib.rs
@@ -47,6 +47,22 @@ fn xor_buffs(buff1: &[u8], buff2: &[u8]) -> Vec<u8> {
         .collect()
 }
 
+fn repeating_key_xor(text: &[u8], key: &[u8]) -> Vec<u8> {
+    text.iter()
+        .zip(key.iter().cycle())
+        .map(|(b, k)| b ^ k)
+        .collect()
+}
+
+#[test]
+fn test_repeating_key_xor() {
+    let sentence = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal";
+    let exp_hex = hex::decode(
+        "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f").unwrap();
+    let cipher = repeating_key_xor(sentence.as_bytes(), b"ICE");
+    assert_eq!(cipher, exp_hex);
+}
+
 #[test]
 fn find_xored_string() {
     let mut best = (String::from(""), 0);
